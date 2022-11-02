31826e2308e7bc99df62b0f6064ace32a18e73c6
Joel Hellberg <95753900+JoelHellberg@users.noreply.github.com>
2022-11-02 16:01:42 +0100

    Create Feed.js

---
+++ src/components/Feed.js
@@ -0,0 +1,12 @@
+// Joel - In progress
+// Denna fil utgör layouten av ett inlägg för 'flödet'
+
+function App() {
+    return (
+      <div className="App">
+  
+      </div>
+    );
+  }
+  
+  export default App;
