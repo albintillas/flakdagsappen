2480b50ce44541b15c47ee15ec6422c5c708cd1b
Hermelin17 <teo_swtor@hotmail.com>
2022-11-02 17:18:38 +0100

    Update avatar & player
    Försökte att göra en class för varje spelare, går sådär...

---
+++ src/components/Avatar.js
@@ -0,0 +1,14 @@
+//Teo
+//import img from 'Images/test.jpeg'
+const Name = " ";
+
+function Avatar({player}) {
+    return (
+      <div>
+        {player.name}
+        <img> src="player.imageSrc </img>
+      </div>
+    );
+  }
+
+export default Avatar;
