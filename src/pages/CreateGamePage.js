489c77a864386b65d77afd7f7f45551281673255
Hermelin17 <teo_swtor@hotmail.com>
2022-11-09 18:09:02 +0100

    Update CreateGamePage.js

--- src/pages/CreateGamePage.js
+++ src/pages/CreateGamePage.js
@@ -1,3 +1,24 @@
-import UploadImage from UploadImage.js
-import TextInput from TextInput.js
-import Button from Button.js
+import UploadImage from '../components/UploadImage.js';
+import TextInput from '../components/TextInput.js';
+import Button from '../components/Button.js';
+
+
+const upload = 
+<UploadImage
+
+/>
+
+const enterName = <TextInput/>
+
+const create =
+<Button
+
+/>
+
+function CreateGamePage(){
+
+    {upload}
+    
+}
+
+export default CreateGamePage;
