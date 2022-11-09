69c58e25b8d260c54e61825b9994431ba015386e
Linus Palm <linus.01@hotmail.com>
2022-11-09 17:50:28 +0100

    Update Button.js

--- src/components/Button.js
+++ src/components/Button.js
@@ -2,17 +2,19 @@
 //in progress
 import React from 'react'
 
-const Button = ({ text, color,  }) =>{
+const Button = ({ text, buttoncolor, textcolor,textdecoration  }) =>{
 
     return(
         <div style={{
-            backgroundColor: color,
-            width: '50%',
+            backgroundColor: buttoncolor,
+            textDecoration: textdecoration,
+            color: textcolor,
             padding: '2% 2% 2% 2%',
             fontSize: '170%',
             fontWeight: 'bold',
-            borderRadius: '4%',
-            textAlign: 'center'
+            borderRadius: '36%',
+            textAlign: 'center',
+            boxShadow: '10px 10px 8px #888888'
           }}>
             {text}
         </div>
