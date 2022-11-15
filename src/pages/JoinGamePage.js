af950d4e82b5d41f4e0bed1a7a103f09d4c6f1af
Linus Palm <linus.01@hotmail.com>
2022-11-15 11:08:50 +0100

    Update JoinGamePage.js

---
+++ src/pages/JoinGamePage.js
@@ -0,0 +1,14 @@
+import React from 'react'
+import PinInput from '../components/PinInput';
+
+function JoinGamePage(){
+   return(
+<div>
+    <p styl={{
+        backgroundColor: 'black'
+    }}>Enter Game Pin!</p>
+</div>
+   ) 
+}
+
+export default JoinGamePage
