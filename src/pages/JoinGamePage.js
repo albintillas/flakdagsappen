2608487ca4c8e520295af44abe8232778df6d911
Linus Palm <linus.01@hotmail.com>
2022-11-15 11:28:25 +0100

    Update JoinGamePage.js

--- src/pages/JoinGamePage.js
+++ src/pages/JoinGamePage.js
@@ -1,12 +1,22 @@
 import React from 'react'
 import PinInput from '../components/PinInput';
+import Button from '../components/Button';
+
+//Linus 
 
 function JoinGamePage(){
    return(
 <div>
-    <p styl={{
-        backgroundColor: 'black'
+    <p style={{
+        backgroundColor: 'white',
+        fontSize:'300%',
+        textDecoration: 'underline',
+        textAlign: 'center'
     }}>Enter Game Pin!</p>
+    <p>PinInput</p>
+    <Button style={{
+        marginRight:'300px' 
+    }}  text='Join Game' buttonColor='#17D930' />
 </div>
    ) 
 }
