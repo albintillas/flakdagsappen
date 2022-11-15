b3457011311851b9c883dbe4bd7477e7bc81a663
daniellaesker <daniel.laesker@live.se>
2022-11-15 14:28:48 +0100

    Update PinInput.js

--- src/components/PinInput.js
+++ src/components/PinInput.js
@@ -1,6 +1,13 @@
 //Laesker
 
-import React from "react"
+
+import React, {useState} from "react"
+import Button from "./Button"
+
+
+
+function PinInput(){
+    const [input, setInput] = useState(""); 
 
 const nums = document.querySelectorAll('.num')
 const form = document.querySelector('form')
@@ -10,27 +17,54 @@ nums.forEach((num, index) => {
     
     num.addEventListener('keyup', () => {
         if (num.value.length == 1) {
-            if (nums[nums.length - 1].value.length == 1) form.submit()
+            if (nums[nums.length - 1].value.length == 1)
             nums[parseInt(num.dataset.id) + 1].focus()
         }
     })
 })
 
+function verifyPassword(event) {  
+   event.preventDefault(); 
+    let pw1 = event.target.value; 
+
+    if (pw1 != 1) {
+        alert ("Password incorrect");
+    }
+    else if (pw1 == 1) {
+        alert ("Password correct");
+    }
+    
+}
 
 
+function validate(){
+    let password = document.getElementById("password").value;
+    if (password == "1"){
+        alert ("password CORRECT");
+        window.location = "passwordCORRECT";
+        return false;
+    } 
+    
 
-function PinInput(){
+}
+
     return(
-        <body style = {{display: 'grid', placeItems: 'center', minHeight: "100vh", backgroundColor: 'rgb(0, 0, 15)'}}>
-            <form action="">
-                <div class="container" style ={{display: 'flex'}}>
-                    <input type="text" class="num" style = {{ color: '#fff',  backgroundColor: 'transparent', width: '100px', textAlign: 'center',  outline: 'none', padding: '2rem 1rem',  margin: '0 1rem', fontSize: '3rem', border: '1px solid rgba(255, 255, 255, .3)', borderRadius: '.5rem', color: 'rgba(0, 255, 0, .5)'}} maxLength="1" required></input>
-                    <input type="text" class="num" style = {{ color: '#fff',  backgroundColor: 'transparent', width: '100px', textAlign: 'center',  outline: 'none', padding: '2rem 1rem',  margin: '0 1rem', fontSize: '3rem', border: '1px solid rgba(255, 255, 255, .3)', borderRadius: '.5rem', color: 'rgba(0, 255, 0, .5)'}} maxLength="1" required></input>
-                    <input type="text" class="num" style = {{ color: '#fff',  backgroundColor: 'transparent', width: '100px', textAlign: 'center',  outline: 'none', padding: '2rem 1rem',  margin: '0 1rem', fontSize: '3rem', border: '1px solid rgba(255, 255, 255, .3)', borderRadius: '.5rem', color: 'rgba(0, 255, 0, .5)'}} maxLength="1" required></input>
-                    <input type="text" class="num" style = {{ color: '#fff',  backgroundColor: 'transparent', width: '100px', textAlign: 'center',  outline: 'none', padding: '2rem 1rem',  margin: '0 1rem', fontSize: '3rem', border: '1px solid rgba(255, 255, 255, .3)', borderRadius: '.5rem', color: 'rgba(0, 255, 0, .5)'}} maxLength="1" required></input>
+        <body style = {{display: 'grid', height: "216vw", backgroundColor: '#F9F3F3'}}>
+            <div style = {{marginTop: '40vw', height: '10vw', fontFamily: 'Brush Script MT', fontSize: '10vw', textDecoration: 'underline'}}>Enter Game Pin!</div>
+            <form style ={{height: "216vw"}}>
+                <div class="container" style ={{display: 'flex', marginTop: '10vw', marginLeft: '10vw', marginRight: '10vw'}}>
+                    
+                    <input type="text"  name="password" id="password" class="num" style = {{ color: '#17D930',  backgroundColor: 'white', height: '15vw', width: '15vw', textAlign: 'center',  outline: 'none', padding: '2vw 1vw',  margin: '0 1vw', fontSize: '10vw', border: '0.5vw solid black', borderRadius: '0.5vw'}} maxLength="1"></input>
+                   
                 </div>
+                <Button text = 'Join Game' type="submit" buttonColor="#17D930" action={validate}></Button>
             </form>
+            
         </body>
     )
 }
+//   onChange={(e) => setInput(e.target.value)}
+
 export default PinInput
+
+
