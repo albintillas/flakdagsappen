f9e5dcd03ac88cdef44a6a5260d2674818bf5d4c
Albin Tillas <albin.tillas@hotmail.com>
2022-11-15 11:31:22 +0100

    Fixed StartPage.js and routes on App.js

--- src/App.js
+++ src/App.js
@@ -1,12 +1,26 @@
 import logo from './logo.svg';
 import './App.css';
+import React from 'react'
+import StartPage from './pages/StartPage.js';
+import JoinGamePage from './pages/JoinGamePage.js';
+import {BrowserRouter, Routes, Route} from "react-router-dom";
 
 function App() {
   return (
-    <div className="App">
+    
+    <BrowserRouter>
+      <Routes>
+        <Route path="/" element={<StartPage/>}/>
+        <Route path="/joingame" element={<JoinGamePage/>}/>
 
-    </div>
+       
+      </Routes>
+    </BrowserRouter>
+
+
   );
 }
 
 export default App;
+
+
