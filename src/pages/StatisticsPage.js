b842d65547173a60c97972891cc80440c7a7452e
Joel Hellberg <95753900+JoelHellberg@users.noreply.github.com>
2022-11-17 11:36:26 +0100

    Update StatisticsPage.js

---
+++ src/pages/StatisticsPage.js
@@ -0,0 +1,10 @@
+import Header from '../components/Header.js';
+
+function StatisticsPage() {
+    return (
+        <div>
+            <Header onpage={1} />
+            </div>
+    );
+}
+export default StatisticsPage;
