955e2608ef108f606fb95bf0d97b8f5e304dac0a
Joel Hellberg <95753900+JoelHellberg@users.noreply.github.com>
2022-11-17 11:36:36 +0100

    Update header.js

--- src/components/header.js
+++ src/components/header.js
@@ -1,20 +1,23 @@
 
 function header({ onpage }) {
     return (
-        <div class="header">
+        <div class="header" style ={{position: "fixed", top: "0", width: "100%"}}>
             <div class="counters"></div>
-            <div class="pages" style ={{textAlign: "center"}}>
+            <div class="pages" style ={{textAlign: "center", width: "100%"}}>
                 <a style={{
                     marginRight: "5vw",
                     ...(onpage == 1
                         ? { textDecoration: "underline" }
                         : {})
                 }}>Statistik</a>
+                <a>l</a>
                 <a style={{
+                    margin: "0 5vw 0 5vw",
                     ...(onpage == 2
                         ? { textDecoration: "underline" }
                         : {})
                 }}>Lobby</a>
+                <a>l</a>
                 <a style={{
                     marginLeft: "5vw",
                     ...(onpage == 3
