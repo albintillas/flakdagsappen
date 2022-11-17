7fdefb3a9b55842aa117bbd157133f8c559395d5
Joel Hellberg <95753900+JoelHellberg@users.noreply.github.com>
2022-11-17 11:36:29 +0100

    Update LobbyPage.js

---
+++ src/pages/LobbyPage.js
@@ -0,0 +1,10 @@
+import Header from '../components/Header.js';
+
+function LobbyPage() {
+    return (
+        <div>
+            <Header onpage={2} />
+            </div>
+    );
+}
+export default LobbyPage;
