400f4914b4013b1ac22efa3bf3f196e45273c950
Joel Hellberg <95753900+JoelHellberg@users.noreply.github.com>
2022-11-22 09:29:45 +0100

    Update FeedPage.js

--- src/pages/FeedPage.js
+++ src/pages/FeedPage.js
@@ -6,61 +6,63 @@ import postImg1 from '..//images/test3.png';
 import postImg2 from '..//images/test2.png';
 import React from 'react';
 
+// In progress, Joel
 function FeedPage() {
     return (
         <div>
             <Header onpage={3} />
-            <Post
-                imageSrc={img1}
-                postContent={"WWWWWWWWWWWWWWWWWWWAAAAWWWWWWWWWWWWWWWWWWAAAWWWWWWWWWWWWWWWWWWAAAWWWWWWWWWWWWWWWWWWW"}
-                isImg={false}
-            />
-            <Post
-                imageSrc={img1}
-                postContent={postImg1}
-                isImg={true}
-            />
-            <Post
-                imageSrc={img2}
-                postContent={'"Ligma Balls" -Big Dick Teo'}
-                isImg={false}
-            />
-            <Post
-                imageSrc={img1}
-                postContent={postImg2}
-                isImg={true}
-            />
-            <Post
-                imageSrc={img1}
-                postContent={"llllllllllllllllllllllllllllllllllllllllllllllllllllllllllllllllllllllllllllllllllllll"}
-                isImg={false}
-            />
-            <Post
-                imageSrc={img1}
-                postContent={"WWWWWWWWWWWWWWWWWWWAAAAWWWWWWWWWWWWWWWWWWAAAWWWWWWWWWWWWWWWWWWAAAWWWWWWWWWWWWWWWWWWW"}
-                isImg={false}
-            />
-            <Post
-                imageSrc={img1}
-                postContent={postImg1}
-                isImg={true}
-            />
-            <Post
-                imageSrc={img2}
-                postContent={'"Ligma Balls" -Big Dick Teo'}
-                isImg={false}
-            />
-            <Post
-                imageSrc={img1}
-                postContent={postImg2}
-                isImg={true}
-            />
-            <Post
-                imageSrc={img1}
-                postContent={"llllllllllllllllllllllllllllllllllllllllllllllllllllllllllllllllllllllllllllllllllllll"}
-                isImg={false}
-            />
-
+            <div class="main" style={{ marginTop: "1vw" }}>
+                <Post
+                    imageSrc={img1}
+                    postContent={"WWWWWWWWWWWWWWWWWWWAAAAWWWWWWWWWWWWWWWWWWAAAWWWWWWWWWWWWWWWWWWAAAWWWWWWWWWWWWWWWWWWW"}
+                    isImg={false}
+                />
+                <Post
+                    imageSrc={img1}
+                    postContent={postImg1}
+                    isImg={true}
+                />
+                <Post
+                    imageSrc={img2}
+                    postContent={'"Ligma Balls" -Big Dick Teo'}
+                    isImg={false}
+                />
+                <Post
+                    imageSrc={img1}
+                    postContent={postImg2}
+                    isImg={true}
+                />
+                <Post
+                    imageSrc={img1}
+                    postContent={"llllllllllllllllllllllllllllllllllllllllllllllllllllllllllllllllllllllllllllllllllllll"}
+                    isImg={false}
+                />
+                <Post
+                    imageSrc={img1}
+                    postContent={"WWWWWWWWWWWWWWWWWWWAAAAWWWWWWWWWWWWWWWWWWAAAWWWWWWWWWWWWWWWWWWAAAWWWWWWWWWWWWWWWWWWW"}
+                    isImg={false}
+                />
+                <Post
+                    imageSrc={img1}
+                    postContent={postImg1}
+                    isImg={true}
+                />
+                <Post
+                    imageSrc={img2}
+                    postContent={'"Ligma Balls" -Big Dick Teo'}
+                    isImg={false}
+                />
+                <Post
+                    imageSrc={img1}
+                    postContent={postImg2}
+                    isImg={true}
+                />
+                <Post
+                    imageSrc={img1}
+                    postContent={"llllllllllllllllllllllllllllllllllllllllllllllllllllllllllllllllllllllllllllllllllllll"}
+                    isImg={false}
+                />
+            </div>
         </div>
     );
 }
