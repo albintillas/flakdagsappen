f384a8cd1b43f9408bc7ac1648aff1c5aa87ca16
Joel Hellberg <95753900+JoelHellberg@users.noreply.github.com>
2022-11-30 15:32:30 +0100

    Update ReturnButton.js

--- src/components/ReturnButton.js
+++ src/components/ReturnButton.js
@@ -5,21 +5,19 @@
 // En knapp som ska backa till tidigare sida
 
 import React from 'react';
-import {Link} from "react-router-dom";
+import { useNavigate } from "react-router-dom";
 import returnArrow from '..//images/backArrow.jpg';
 
-const Button = ({action }) =>{
+const Button = ({ action }) => {
+    const navigate = useNavigate();
+    return (
 
-    return(
+        <img src={returnArrow} alt='Return' onClick={() => navigate(-1)} style={{
+            width: "12vw"
+        }}>
+        </img>
 
-        <Link to={"/" + action}>
-            <img src={returnArrow} alt='Return' onclick = {action} style={{
-                width: "12vw"
-            }}>
-            </img>
-        </Link>
 
-    
     )
 }
 export default Button
