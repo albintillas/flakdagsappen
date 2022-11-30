d0407ee651eb4804e46008b429da72092b3379db
daniellaesker <daniel.laesker@live.se>
2022-11-30 08:03:29 +0100

    Update JoinGamePage.js

--- src/pages/JoinGamePage.js
+++ src/pages/JoinGamePage.js
@@ -9,8 +9,21 @@ function JoinGamePage(){
    return(
 <div>
    <ReturnButton action=""></ReturnButton>
-    <p>Enter Game Pin!</p>
+   <div style={{display: 'flex', flexDirection: 'row', justifyContent: 'center'}}>
+      <h2 style={{fontSize: '5vw', width: '40vw'}}>Enter Name:</h2>
+      <input className="playerNameInput" style={{
+         width: '40vw'
+      }}>
+      </input>
+   </div>
+   <div style={{display: 'flex', flexDirection: 'row', justifyContent: 'center'}}>
+      <h2 style={{fontSize: '5vw', width: '40vw'}}>Enter Password:</h2>
+      <input classname="playerPasswordInput" style={{
+         width: '40vw'
+      }}></input>
 
+   </div>
+    <p>Enter Game Pin!</p>
     <PinInput></PinInput>
 </div>
    ) 
